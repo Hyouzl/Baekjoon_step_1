2a7ae149507cfd14294e3c2de3c612a1b3a95e70
yuri-note <yuri-note@LAPTOP-NI438OED>
2022-09-05 19:55:21 +0900

    rere

--- Baekjoon_step_1/src/step_1/Baekjoon3003.java
+++ Baekjoon_step_1/src/step_1/Baekjoon3003.java
@@ -14,10 +14,53 @@ public class Baekjoon3003 {
 		int knight = scan.nextInt();
 		int pwun = scan.nextInt();
 		
-
+		if (king > 1) {
+			System.out.println(-(king - 1));
+		}
+		else if (king < 1) {
+			System.out.println(1 - king);
+		}
+		else System.out.println(0);
 		
+		if (queen > 1) {
+			System.out.println(-(queen - 1));
+		}
+		else if (queen < 1) {
+			System.out.println(1 - queen);
+		}
+		else System.out.println(0);
 		
+		if (look > 2) {
+			System.out.println(-(look - 2));
+		}
+		else if (look < 2) {
+			System.out.println(2 - look);
+		}
+		else System.out.println(0);
 		
+		if (bishop > 2) {
+			System.out.println(-(bishop - 2));
+		}
+		else if (bishop < 2) {
+			System.out.println(2 - bishop);
+		}
+		else System.out.println(0);
+		
+		if (knight > 2) {
+			System.out.println(-(knight - 2));
+		}
+		else if (knight < 2) {
+			System.out.println(2 - knight);
+		}
+		else System.out.println(0);
+		
+		if (pwun > 8) {
+			System.out.println(-(pwun - 8));
+		}
+		else if (pwun < 8) {
+			System.out.println(8 - pwun);
+		}
+		else System.out.println(0);
 	}
 
 }
